5e16b9c8ebd891e707e32e2b74814d8518f779a9
Bogdan Tomic <boki.tomic001@gmail.com>
2024-12-25 20:26:55 +0100

    test changes/dont mind me

--- app/page.jsx
+++ app/page.jsx
@@ -1,4 +1,4 @@
-import Link from 'next/link';
+import Link from "next/link";
 export default function Home() {
   return (
     <>
@@ -6,12 +6,12 @@ export default function Home() {
         <div className="text-center">
           <h1 className="text-7xl font-bold text-indigo-700 mb-6">Quizzer</h1>
           <p className="text-gray-600 mb-8">
-            Welcome to the ultimate quiz experience!
+            Welcome to the ultimate quiz experience!!!!!!!!
           </p>
           <Link href="/game">
-          <button className="bg-indigo-700 text-white px-6 py-3 rounded-md mr-4 hover:bg-indigo-400 focus:outline-none focus:ring focus:border-indigo-300 transition duration-300 ease-in-out">
-            Play Now
-          </button>
+            <button className="bg-indigo-700 text-white px-6 py-3 rounded-md mr-4 hover:bg-indigo-400 focus:outline-none focus:ring focus:border-indigo-300 transition duration-300 ease-in-out">
+              Play Now
+            </button>
           </Link>
           <a
             href="https://github.com/bogdantomic1"
